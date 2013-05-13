abf65c9a0045c928e49bff7170ee07220d9e81b1
hyun woo kwon <hyunk@xilinx.com>
2013-05-13 13:48:39 -0700

    drm: zynq_drm: Add a zynq_drm placeholder
    Add ZYNQ_DRM to defconfig. Add zynq drm to Kconfigs, Makefiles
    and an empty source code.
    
    Signed-off-by: hyun woo kwon <hyunk@xilinx.com>

---
+++ drivers/gpu/drm/zynq/zynq_drm_drv.c
@@ -0,0 +1,15 @@
+/*
+ * Xilinx DRM KMS support for Zynq
+ *
+ * Copyright (C) 2013 Xilinx, Inc. All rights reserved.
+ * Author: hyun woo kwon<hyunk@xilinx.com>
+ *
+ * Description:
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
