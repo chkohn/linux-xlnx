d856410414d7ffc1168e6e874ba4c2f6225db4a6
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2013-05-23 13:48:17 +0200

    dma: xilinx: vdma: Configure the hardware when starting the transfer
    Set the XILINX_VDMA_REG_HSIZE and XILINX_VDMA_REG_FRMDLY_STRIDE
    registers when starting the transfer, not when preparing the request.
    This is needed to allow several requests to be prepared and submitted
    for execution.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/dma/xilinx/xilinx_axivdma.c
+++ drivers/dma/xilinx/xilinx_axivdma.c
@@ -510,11 +510,18 @@ static void xilinx_vdma_start_transfer(struct xilinx_vdma_chan *chan)
 		      XILINX_VDMA_DMAXR_ALL_IRQ_MASK);
 
 	/* Start the transfer */
-	if (chan->has_SG)
+	if (chan->has_SG) {
 		vdma_ctrl_write(chan, XILINX_VDMA_REG_TAILDESC,
 				desct->async_tx.phys);
-	else
+	} else {
+		vdma_desc_write(chan, XILINX_VDMA_REG_HSIZE, config->hsize);
+		vdma_desc_write(chan, XILINX_VDMA_REG_FRMDLY_STRIDE,
+				(config->frm_dly <<
+				 XILINX_VDMA_FRMDLY_STRIDE_FRMDLY_SHIFT) |
+				(config->stride <<
+				 XILINX_VDMA_FRMDLY_STRIDE_STRIDE_SHIFT));
 		vdma_desc_write(chan, XILINX_VDMA_REG_VSIZE, config->vsize);
+	}
 
 out_unlock:
 	spin_unlock_irqrestore(&chan->lock, flags);
@@ -791,15 +798,6 @@ static struct dma_async_tx_descriptor *xilinx_vdma_prep_slave_sg(
 			sg_len, chan->num_frms);
 
 		return NULL;
-	}
-
-	if (!chan->has_SG) {
-		vdma_desc_write(chan, XILINX_VDMA_REG_HSIZE, chan->config.hsize);
-		vdma_desc_write(chan, XILINX_VDMA_REG_FRMDLY_STRIDE,
-				(chan->config.frm_dly <<
-				 XILINX_VDMA_FRMDLY_STRIDE_FRMDLY_SHIFT) |
-				(chan->config.stride <<
-				 XILINX_VDMA_FRMDLY_STRIDE_STRIDE_SHIFT));
 	}
 
 	/* Build transactions using information in the scatter gather list */
