d88f982e4c5a206baee6f0d29e8b8b6d3f01c8eb
hyun woo kwon <hyunk@xilinx.com>
2013-07-17 10:21:54 -0700

    drm: zynq_drm: Remove empty descriptions
    Remove empty descriptions.
    
    Signed-off-by: hyun woo kwon <hyunk@xilinx.com>

--- drivers/gpu/drm/zynq/zynq_drm_drv.h
+++ drivers/gpu/drm/zynq/zynq_drm_drv.h
@@ -4,8 +4,6 @@
  * Copyright (C) 2013 Xilinx, Inc. All rights reserved.
  * Author: hyun woo kwon <hyunk@xilinx.com>
  *
- * Description:
- *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
