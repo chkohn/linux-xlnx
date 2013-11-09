1905baf69af9a191bc8863f9e3d0705b411ee773
Hyun Kwon <hyunk@xilinx.com>
2013-11-08 16:55:26 -0800

    media: xilinx: Remove _TIMING_ from register definition
    Remove _TIMING_ from register definition.

--- drivers/media/platform/xilinx/xilinx-enhance.c
+++ drivers/media/platform/xilinx/xilinx-enhance.c
@@ -79,9 +79,9 @@ static int xenhance_s_stream(struct v4l2_subdev *subdev, int enable)
 		return 0;
 	}
 
-	xvip_write(&xenhance->xvip, XVIP_TIMING_ACTIVE_SIZE,
-		   (height << XVIP_TIMING_ACTIVE_VSIZE_SHIFT) |
-		   (width << XVIP_TIMING_ACTIVE_HSIZE_SHIFT));
+	xvip_write(&xenhance->xvip, XVIP_ACTIVE_SIZE,
+		   (height << XVIP_ACTIVE_VSIZE_SHIFT) |
+		   (width << XVIP_ACTIVE_HSIZE_SHIFT));
 
 	xvip_write(&xenhance->xvip, XVIP_CTRL_CONTROL,
 		   XVIP_CTRL_CONTROL_SW_ENABLE | XVIP_CTRL_CONTROL_REG_UPDATE);
--- drivers/media/platform/xilinx/xilinx-stats.c
+++ drivers/media/platform/xilinx/xilinx-stats.c
@@ -89,9 +89,9 @@ static int xstats_s_stream(struct v4l2_subdev *subdev, int enable)
 		return 0;
 	}
 
-	xvip_write(&xstats->xvip, XVIP_TIMING_ACTIVE_SIZE,
-		   (height << XVIP_TIMING_ACTIVE_VSIZE_SHIFT) |
-		   (width << XVIP_TIMING_ACTIVE_HSIZE_SHIFT));
+	xvip_write(&xstats->xvip, XVIP_ACTIVE_SIZE,
+		   (height << XVIP_ACTIVE_VSIZE_SHIFT) |
+		   (width << XVIP_ACTIVE_HSIZE_SHIFT));
 
 	xvip_write(&xstats->xvip, XVIP_CTRL_CONTROL,
 		   XVIP_CTRL_CONTROL_SW_ENABLE | XVIP_CTRL_CONTROL_REG_UPDATE);
