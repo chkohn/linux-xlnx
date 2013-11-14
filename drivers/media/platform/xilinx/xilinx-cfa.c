86cbff2a4e77f594f3f2fb081f86ef06c3191c54
Hyun Kwon <hyunk@xilinx.com>
2013-11-13 16:20:20 -0800

    media: xilinx: cfa: Fix enum_frame_size()
    Fix enum_frame_size().

--- drivers/media/platform/xilinx/xilinx-cfa.c
+++ drivers/media/platform/xilinx/xilinx-cfa.c
@@ -122,7 +122,7 @@ static int xcfa_enum_frame_size(struct v4l2_subdev *subdev,
 		fse->min_height = XCFA_MIN_HEIGHT;
 		fse->max_height = XCFA_MAX_HEIGHT;
 	} else {
-		/* The size on the source pad are fixed and always identical to
+		/* The size on the source pad is fixed and always identical to
 		 * the size on the sink pad.
 		 */
 		fse->min_width = format->width;
