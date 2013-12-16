2fb5d5c44b51d92bd219f3c665d90ccac3900171
Hyun Kwon <hyunk@xilinx.com>
2013-12-16 14:44:46 -0800

    media: xilinx: tpg: Remove _TIMING_ from register definition
    Remove _TIMING_ from register definition.
    
    Signed-off-by: Hyun Kwon <hyunk@xilinx.com>

--- drivers/media/platform/xilinx/xilinx-tpg.c
+++ drivers/media/platform/xilinx/xilinx-tpg.c
@@ -110,9 +110,9 @@ static int xtpg_s_stream(struct v4l2_subdev *subdev, int enable)
 		return 0;
 	}
 
-	xvip_write(&xtpg->xvip, XVIP_TIMING_ACTIVE_SIZE,
-		   (height << XVIP_TIMING_ACTIVE_VSIZE_SHIFT) |
-		   (width << XVIP_TIMING_ACTIVE_HSIZE_SHIFT));
+	xvip_write(&xtpg->xvip, XVIP_ACTIVE_SIZE,
+		   (height << XVIP_ACTIVE_VSIZE_SHIFT) |
+		   (width << XVIP_ACTIVE_HSIZE_SHIFT));
 
 	xvip_write(&xtpg->xvip, XVIP_CTRL_CONTROL, XVIP_CTRL_CONTROL_SW_ENABLE |
 		   XVIP_CTRL_CONTROL_REG_UPDATE);
