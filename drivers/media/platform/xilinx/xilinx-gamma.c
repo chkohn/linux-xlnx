62ccd4488f660c67d640b19effaa9ba09da56d9a
Hyun Kwon <hyunk@xilinx.com>
2013-12-19 13:38:00 -0800

    media: xilinx: gamma: Use xvip helper functions
    Use xvip helper functions.
    
    This patch will be squahsed into the initial one.

--- drivers/media/platform/xilinx/xilinx-gamma.c
+++ drivers/media/platform/xilinx/xilinx-gamma.c
@@ -27,14 +27,6 @@
 #include "xilinx-controls.h"
 #include "xilinx-vip.h"
 
-#define XGAMMA_MIN_WIDTH				32
-#define XGAMMA_MAX_WIDTH				7680
-#define XGAMMA_MIN_HEIGHT				32
-#define XGAMMA_MAX_HEIGHT				7680
-
-#define XGAMMA_PAD_SINK					0
-#define XGAMMA_PAD_SOURCE				1
-
 #define XGAMMA_GAMMA_TABLE_UPDATE			0x100
 #define XGAMMA_GAMMA_ADDR_DATA				0x104
 
@@ -70,18 +62,13 @@ static int xgamma_s_stream(struct v4l2_subdev *subdev, int enable)
 	const u32 height = xgamma->format.height;
 
 	if (!enable) {
-		xvip_write(&xgamma->xvip, XVIP_CTRL_CONTROL,
-			   XVIP_CTRL_CONTROL_SW_RESET);
-		xvip_write(&xgamma->xvip, XVIP_CTRL_CONTROL, 0);
+		xvip_stop(&xgamma->xvip);
 		return 0;
 	}
 
-	xvip_write(&xgamma->xvip, XVIP_ACTIVE_SIZE,
-		   (height << XVIP_ACTIVE_VSIZE_SHIFT) |
-		   (width << XVIP_ACTIVE_HSIZE_SHIFT));
+	xvip_set_size(&xgamma->xvip, width, height);
 
-	xvip_write(&xgamma->xvip, XVIP_CTRL_CONTROL,
-		   XVIP_CTRL_CONTROL_SW_ENABLE | XVIP_CTRL_CONTROL_REG_UPDATE);
+	xvip_start(&xgamma->xvip);
 
 	return 0;
 }
@@ -90,72 +77,14 @@ static int xgamma_s_stream(struct v4l2_subdev *subdev, int enable)
  * V4L2 Subdevice Pad Operations
  */
 
-static int xgamma_enum_mbus_code(struct v4l2_subdev *subdev,
-				 struct v4l2_subdev_fh *fh,
-				 struct v4l2_subdev_mbus_code_enum *code)
-{
-	struct xgamma_device *xgamma = to_gamma(subdev);
-
-	if (code->index)
-		return -EINVAL;
-
-	code->code = xgamma->vip_format->code;
-
-	return 0;
-}
-
-static int xgamma_enum_frame_size(struct v4l2_subdev *subdev,
-				  struct v4l2_subdev_fh *fh,
-				  struct v4l2_subdev_frame_size_enum *fse)
-{
-	struct v4l2_mbus_framefmt *format;
-
-	format = v4l2_subdev_get_try_format(fh, fse->pad);
-
-	if (fse->index || fse->code != format->code)
-		return -EINVAL;
-
-	if (fse->pad == XGAMMA_PAD_SINK) {
-		fse->min_width = XGAMMA_MIN_WIDTH;
-		fse->max_width = XGAMMA_MAX_WIDTH;
-		fse->min_height = XGAMMA_MIN_HEIGHT;
-		fse->max_height = XGAMMA_MAX_HEIGHT;
-	} else {
-		/* The size on the source pad is fixed and always identical to
-		 * the size on the sink pad.
-		 */
-		fse->min_width = format->width;
-		fse->max_width = format->width;
-		fse->min_height = format->height;
-		fse->max_height = format->height;
-	}
-
-	return 0;
-}
-
-static struct v4l2_mbus_framefmt *
-__xgamma_get_pad_format(struct xgamma_device *xgamma,
-			struct v4l2_subdev_fh *fh,
-			unsigned int pad, u32 which)
-{
-	switch (which) {
-	case V4L2_SUBDEV_FORMAT_TRY:
-		return v4l2_subdev_get_try_format(fh, pad);
-	case V4L2_SUBDEV_FORMAT_ACTIVE:
-		return &xgamma->format;
-	default:
-		return NULL;
-	}
-}
-
 static int xgamma_get_format(struct v4l2_subdev *subdev,
 			     struct v4l2_subdev_fh *fh,
 			     struct v4l2_subdev_format *fmt)
 {
 	struct xgamma_device *xgamma = to_gamma(subdev);
 
-	fmt->format = *__xgamma_get_pad_format(xgamma, fh, fmt->pad,
-					       fmt->which);
+	fmt->format = *xvip_get_pad_format(fh, &xgamma->format, fmt->pad,
+					   fmt->which);
 
 	return 0;
 }
@@ -167,24 +96,22 @@ static int xgamma_set_format(struct v4l2_subdev *subdev,
 	struct xgamma_device *xgamma = to_gamma(subdev);
 	struct v4l2_mbus_framefmt *__format;
 
-	__format = __xgamma_get_pad_format(xgamma, fh, fmt->pad, fmt->which);
+	__format = xvip_get_pad_format(fh, &xgamma->format, fmt->pad,
+				       fmt->which);
 
-	if (fmt->pad == XGAMMA_PAD_SOURCE) {
+	if (fmt->pad == XVIP_PAD_SOURCE) {
 		fmt->format = *__format;
 		return 0;
 	}
 
-	__format->code = xgamma->vip_format->code;
-	__format->width = clamp_t(unsigned int, fmt->format.width,
-				  XGAMMA_MIN_WIDTH, XGAMMA_MAX_WIDTH);
-	__format->height = clamp_t(unsigned int, fmt->format.height,
-				   XGAMMA_MIN_HEIGHT, XGAMMA_MAX_HEIGHT);
+	xvip_set_format(__format, xgamma->vip_format, fmt);
 
 	fmt->format = *__format;
 
 	/* Propagate the format to the source pad. */
-	__format = __xgamma_get_pad_format(xgamma, fh, XGAMMA_PAD_SOURCE,
-					   fmt->which);
+	__format = xvip_get_pad_format(fh, &xgamma->format, XVIP_PAD_SOURCE,
+				       fmt->which);
+
 	*__format = fmt->format;
 
 	return 0;
@@ -194,44 +121,9 @@ static int xgamma_set_format(struct v4l2_subdev *subdev,
  * V4L2 Subdevice Operations
  */
 
-/**
- * xgamma_init_formats - Initialize formats on all pads
- * @subdev: gammaper V4L2 subdevice
- * @fh: V4L2 subdev file handle
- *
- * Initialize all pad formats with default values. If fh is not NULL, try
- * formats are initialized on the file handle. Otherwise active formats are
- * initialized on the device.
- */
-static void xgamma_init_formats(struct v4l2_subdev *subdev,
-				struct v4l2_subdev_fh *fh)
-{
-	struct xgamma_device *xgamma = to_gamma(subdev);
-	struct v4l2_subdev_format format;
-
-	memset(&format, 0, sizeof(format));
-
-	format.which = fh ? V4L2_SUBDEV_FORMAT_TRY : V4L2_SUBDEV_FORMAT_ACTIVE;
-	format.format.width = xvip_read(&xgamma->xvip, XVIP_ACTIVE_SIZE) &
-			      XVIP_ACTIVE_HSIZE_MASK;
-	format.format.height = (xvip_read(&xgamma->xvip, XVIP_ACTIVE_SIZE) &
-				XVIP_ACTIVE_VSIZE_MASK) >>
-			       XVIP_ACTIVE_VSIZE_SHIFT;
-	format.format.field = V4L2_FIELD_NONE;
-	format.format.colorspace = V4L2_COLORSPACE_SRGB;
-
-	format.pad = XGAMMA_PAD_SINK;
-
-	xgamma_set_format(subdev, fh, &format);
-
-	format.pad = XGAMMA_PAD_SOURCE;
-
-	xgamma_set_format(subdev, fh, &format);
-}
-
 static int xgamma_open(struct v4l2_subdev *subdev, struct v4l2_subdev_fh *fh)
 {
-	xgamma_init_formats(subdev, fh);
+	xvip_init_formats(subdev, fh);
 
 	return 0;
 }
@@ -279,8 +171,8 @@ static struct v4l2_subdev_video_ops xgamma_video_ops = {
 };
 
 static struct v4l2_subdev_pad_ops xgamma_pad_ops = {
-	.enum_mbus_code		= xgamma_enum_mbus_code,
-	.enum_frame_size	= xgamma_enum_frame_size,
+	.enum_mbus_code		= xvip_enum_mbus_code,
+	.enum_frame_size	= xvip_enum_frame_size,
 	.get_fmt		= xgamma_get_format,
 	.set_fmt		= xgamma_set_format,
 };
@@ -410,10 +302,10 @@ static int xgamma_probe(struct platform_device *pdev)
 	v4l2_set_subdevdata(subdev, xgamma);
 	subdev->flags |= V4L2_SUBDEV_FL_HAS_DEVNODE;
 
-	xgamma_init_formats(subdev, NULL);
+	xvip_init_formats(subdev, NULL);
 
-	xgamma->pads[XGAMMA_PAD_SINK].flags = MEDIA_PAD_FL_SINK;
-	xgamma->pads[XGAMMA_PAD_SOURCE].flags = MEDIA_PAD_FL_SOURCE;
+	xgamma->pads[XVIP_PAD_SINK].flags = MEDIA_PAD_FL_SINK;
+	xgamma->pads[XVIP_PAD_SOURCE].flags = MEDIA_PAD_FL_SOURCE;
 	subdev->entity.ops = &xgamma_media_ops;
 	ret = media_entity_init(&subdev->entity, 2, xgamma->pads, 0);
 	if (ret < 0)
